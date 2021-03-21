59cc506d31dc140289958382bf0c27aa0161d894
donnels <github@donnellan.de>
2021-03-21 20:24:24 +0100

    experiments with chromium

---
+++ html2x/chromium.sh
@@ -0,0 +1,16 @@
+# chromium \
+#     --no-sandbox \
+#     --headless \
+#     --disable-gpu \
+#     --use-gl=swiftshader \
+#     --print-to-pdf \
+#     --remote-debugging-port=8888 \
+#     https://donnels.github.io
+
+chromium \
+    --no-sandbox \
+    --headless \
+    --disable-gpu \
+    --use-gl=swiftshader \
+    --remote-debugging-port=8888 \
+    https://donnels.github.io

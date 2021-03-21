5ca60de36a34cf9cd2667130dbb47114b89244b9
donnels <github@donnellan.de>
2021-03-21 21:11:26 +0100

    css tests with chromium

--- html2x/chromium.sh
+++ html2x/chromium.sh
@@ -1,16 +1,17 @@
-# chromium \
-#     --no-sandbox \
-#     --headless \
-#     --disable-gpu \
-#     --use-gl=swiftshader \
-#     --print-to-pdf=/data/test.pdf \
-#     --remote-debugging-port=8888 \
-#     https://donnels.github.io
-
 chromium \
     --no-sandbox \
     --headless \
     --disable-gpu \
     --use-gl=swiftshader \
-    --remote-debugging-port=8888 \
+    --run-all-compositor-stages-before-draw \
+    --disable-extensions \
+    --print-to-pdf=/data/test.pdf \
     https://donnels.github.io
+
+# chromium \
+#     --no-sandbox \
+#     --headless \
+#     --disable-gpu \
+#     --use-gl=swiftshader \
+#     --remote-debugging-port=8888 \
+#     https://donnels.github.io

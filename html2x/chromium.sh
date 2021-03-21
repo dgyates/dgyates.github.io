f9c23aad1c3eb74a143da1dc9ad13bb6fd51575d
donnels <github@donnellan.de>
2021-03-21 21:23:08 +0100

    testing with 1/2cm margin

--- html2x/chromium.sh
+++ html2x/chromium.sh
@@ -2,9 +2,6 @@ chromium \
     --no-sandbox \
     --headless \
     --disable-gpu \
-    --use-gl=swiftshader \
-    --run-all-compositor-stages-before-draw \
-    --disable-extensions \
     --print-to-pdf=/data/test.pdf \
     https://donnels.github.io
 
@@ -12,6 +9,8 @@ chromium \
 #     --no-sandbox \
 #     --headless \
 #     --disable-gpu \
+#     --disable-extensions \
+#v    --run-all-compositor-stages-before-draw \
 #     --use-gl=swiftshader \
 #     --remote-debugging-port=8888 \
 #     https://donnels.github.io
